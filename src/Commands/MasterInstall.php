3b7c87fc943c9aef109c85dce913feb17a67c9a9
hamizannorwan <110874395+mizanchan22@users.noreply.github.com>
2025-07-02 16:21:52 +0800

    first commit

---
+++ src/Commands/MasterInstall.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace IM1\MasterInstaller\Commands;
+
+use CodeIgniter\CLI\BaseCommand;
+use CodeIgniter\CLI\CLI;
+
+class MasterInstall extends BaseCommand
+{
+    protected $group       = 'IM1';
+    protected $name        = 'im1:run';
+    protected $description = 'Run the IM1 Master Installer Utk Dapat Semua Library';
+
+    public function run(array $params)
+    {
+        CLI::newLine();
+        CLI::write("                                    ':.                                         ", 'light_yellow');
+        CLI::write("                                       []_____                                  ", 'light_yellow');
+        CLI::write("                                      /\\      \\                                ", 'light_yellow');
+        CLI::write("                                  ___/  \\__/\\__\\__                             ", 'light_yellow');
+        CLI::write("                             ---/\\___\\ |''''''|__\\---                       ", 'light_yellow');
+        CLI::write("                                ||'''| |''||''|''|                              ", 'white');
+        CLI::write("                                ``\"\"\"`\"`\"))\"\"`\"\"``                              ", 'light_yellow');
+
+            CLI::newLine();
+        CLI::write('██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗    ██╗███╗   ███╗ ██╗', 'light_blue');
+        CLI::write('██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝    ██║████╗ ████║███║', 'light_green');
+        CLI::write('██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗      ██║██╔████╔██║╚██║', 'light_green');
+        CLI::write('██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝      ██║██║╚██╔╝██║ ██║', 'light_green');
+        CLI::write('╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗    ██║██║ ╚═╝ ██║ ██║', 'light_green');
+        CLI::write(' ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝    ╚═╝╚═╝     ╚═╝ ╚═╝', 'light_blue');
+        CLI::newLine();
+
+        CLI::write(str_pad('Entertainment - Development - Productivity', 85, ' ', STR_PAD_BOTH), 'yellow');
+        CLI::write(str_pad('& much more!', 85, ' ', STR_PAD_BOTH), 'light_green');
+        CLI::write('');
+
+        CLI::newLine();
+
+        $option = CLI::prompt('Select option', [
+            'Install Template',
+            'Install Module',
+            'Exit'
+        ]);
+
+        switch ($option) {
+            case 'Install Theme':
+                CLI::write('Installing Theme...');
+                \CodeIgniter\CLI\Commands::run('im1:theme');
+                break;
+
+            case 'Install Module':
+                CLI::write('Installing Module...');
+                break;
+
+            case 'Exit':
+                CLI::write('Goodbye!');
+                break;
+        }
+    }
+}
